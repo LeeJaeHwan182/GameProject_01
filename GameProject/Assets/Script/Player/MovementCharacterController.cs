b77810eb16d400f0c542ff9c726f3dfb6d2fa21d
LeeJaeHwan <a5039981@naver.com>
2022-03-27 22:18:14 +0900

    (LJH) PlayerMovement_2022_03_27

---
+++ GameProject/Assets/Script/Player/MovementCharacterController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementCharacterController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed; //이동속도
+    private Vector3 moveForce; //이동 힘 (x, z와 y축을 별도로 계산해 실제 이동에 적용)
+
+    [SerializeField]
+    private float jumpForce; //점프 힘
+    [SerializeField]
+    private float gravity; //중력 계수
+
+    public float Movespeed
+    {
+        set => moveSpeed = Mathf.Max(0, value);
+        get => moveSpeed;
+    }
+
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // 허공에 떠있으면 중력만큼 y축 이동속도 감소
+        if( !characterController.isGrounded )
+        {
+            moveForce.y += gravity * Time.deltaTime;
+        }
+
+        // 1초당 moveForce 속력으로 이동
+        characterController.Move(moveForce * Time.deltaTime);
+    }
+
+    public void MoveTo(Vector3 direction)
+    {
+        // 이동 방향 = 캐릭터의 회전 값 * 방향 값
+        direction = transform.rotation * new Vector3(direction.x, 0, direction.z);
+
+        // 이동 힘 = 이동방향 * 속도
+        moveForce = new Vector3(direction.x * moveSpeed, moveForce.y, direction.z * moveSpeed);
+    }
+
+    public void Jump()
+    {
+        //플레이어가 바닥에 있을 때만 점프 가능
+        if( characterController.isGrounded )
+        {
+            moveForce.y = jumpForce;
+        }
+    }
+}
--- GameProject/Assets/Script/Player/PlayerController.cs
+++ GameProject/Assets/Script/Player/PlayerController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Input KeyCodes")]
+    [SerializeField] private KeyCode keyCodeRun = KeyCode.LeftShift; //달리기 키
+    [Header("Input KeyCodes")]
+    [SerializeField] private KeyCode keyCodeJump = KeyCode.Space; //점프 키
+
     private RotateToMouse rotateToMouse; // 마우스 이동으로 카메라 회전
+    private MovementCharacterController movement; // 키보드 입력으로 플레이어 이동, 점프
+    private Status status;
 
     private void Awake()
     {
@@ -13,11 +20,15 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
 
         rotateToMouse = GetComponent<RotateToMouse>();
+        movement = GetComponent<MovementCharacterController>();
+        status = GetComponent<Status>();
     }
 
     private void Update()
     {
         UpdateRotate();
+        UpdateMove();
+        UpdateJump();
     }
 
     private void UpdateRotate()
@@ -26,5 +37,31 @@ public class PlayerController : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y");
 
         rotateToMouse.UpdateRotate(mouseX, mouseY);
+    }
+
+    private void UpdateMove()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        // 이동중 일 때 (걷기 or 뛰기)
+        if (x != 0 || z != 0)
+        {
+            bool isRun = false;
+
+            // 옆이나 뒤로 이동할 때는 달릴 수 없다
+            if (z > 0) isRun = Input.GetKey(keyCodeRun);
+
+            movement.Movespeed = isRun == true ? status.RunSpeed : status.WalkSpeed;
+        }
+        movement.MoveTo(new Vector3(x, 0, z));
+    }
+
+    private void UpdateJump()
+    {
+        if(Input.GetKeyDown(keyCodeJump))
+        {
+            movement.Jump();
+        }
     }
 }
